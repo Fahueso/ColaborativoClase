275d10a9d68c867674cdf5373c7b883494d08dfd
fahueso <fa.huesopastor@edu.gva.es>
2025-12-16 08:56:59 +0100

    Esqueleto con 3/19 alumnos para pasar lista

---
+++ src/TrabajoClase.java
@@ -0,0 +1,10 @@
+public class TrabajoClase {
+    public static void main(String[] args) {
+        System.out.println("Pasando Lista:");
+        System.out.println("Cristian:");
+        System.out.println("Robert:");
+        System.out.println("Claudia:");
+
+
+    }
+}
