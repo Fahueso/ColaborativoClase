b83d85c8a4547b3a1705bffb241a902660020a58
robbicmoy <robbicmoy@alu.edu.gva.es>
2025-12-16 09:11:48 +0100

    Trabajo de Robert

--- src/TrabajoClase.java
+++ src/TrabajoClase.java
@@ -2,7 +2,7 @@ public class TrabajoClase {
     public static void main(String[] args) {
         System.out.println("Pasando Lista:");
         System.out.println("Cristian:");
-        System.out.println("Robert:");
+        System.out.println("Robert: Presente");
         System.out.println("Claudia:");
 
 
